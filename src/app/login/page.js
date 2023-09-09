97c44a198f4bcad4713903a9b5f52b2ca73eef67
Your Name <ekka.beaula@gmail.com>
2023-09-09 23:48:08 +0200

    vercelfix5

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,9 +1,9 @@
 import LoginForm from "../components/LoginForm";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const page = async () => {
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
   return (
     <div>
