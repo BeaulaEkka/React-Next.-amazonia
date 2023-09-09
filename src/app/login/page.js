d3ab6abf3cc419fc26965d71580184cdd72067c3
Your Name <ekka.beaula@gmail.com>
2023-09-10 00:15:58 +0200

    6

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,10 +1,10 @@
 import LoginForm from "../components/LoginForm";
-import { getServerSession } from "next-auth";
-import { authOptions } from "../api/auth/[...nextauth]/route";
+// import { getServerSession } from "next-auth";
+// import { authOptions } from "../api/auth/[...nextauth]/route";
 
 const page = async () => {
   // The 'session' variable is fetched here but not used in this file.
-  const session = await getServerSession(authOptions);
+  // const session = await getServerSession(authOptions);
 
   return (
     <div>
