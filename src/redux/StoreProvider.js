89ce693ee707c90e56d5efdff7badc928ee1d9b3
Your Name <ekka.beaula@gmail.com>
2023-09-10 01:16:04 +0200

    10

--- src/redux/StoreProvider.js
+++ src/redux/StoreProvider.js
@@ -3,5 +3,9 @@ import { store } from "./store";
 import { Provider } from "react-redux";
 
 export function StoreProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store} basePath="/api/auth">
+      {children}
+    </Provider>
+  );
 }
