54abb95c6d7dc076a4b7ca0553a414f2e3e3892a
Your Name <ekka.beaula@gmail.com>
2023-09-11 00:44:48 +0200

    vercel17

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,20 +1,60 @@
 import bcrypt from "bcrypt";
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
+// import { connectMongoDB } from "@/lib/mongodb";
+// import User from "@/models/user";
+
+// export async function POST(req) {
+//   try {
+//     const { name, email, password } = await req.json();
+//     const hashedPassword = await bcrypt.hash(password, 10);
+//     await connectMongoDB();
+//     await User.create({ name, email, password: hashedPassword });
+//     console.log("User", User);
+//     return NextResponse.json({ message: "User registered." }, { status: 201 });
+//   } catch (error) {
+//     return NextResponse.json(
+//       { message: "An error occured while registering the user. Try again." },
+//       { status: 500 }
+//     );
+//   }
+// }
+// api/register.js
+
 import { connectMongoDB } from "@/lib/mongodb";
 import User from "@/models/user";
 
-export async function POST(req) {
-  try {
-    const { name, email, password } = await req.json();
-    const hashedPassword = await bcrypt.hash(password, 10);
-    await connectMongoDB();
-    await User.create({ name, email, password: hashedPassword });
-    console.log("User", User);
-    return NextResponse.json({ message: "User registered." }, { status: 201 });
-  } catch (error) {
-    return NextResponse.json(
-      { message: "An error occured while registering the user. Try again." },
-      { status: 500 }
-    );
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      await connectMongoDB();
+      const { name, email, password } = req.body;
+
+      // Check if the user already exists
+      const userExists = await User.findOne({ email });
+
+      if (userExists) {
+        return res.status(400).json({ error: "User already exists" });
+      }
+
+      // Hash the user's password (use a secure hashing library)
+      const hashedPassword = await bcrypt.hash(password, 10);
+
+      // Create a new user document and save it to the database
+      const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+      });
+
+      await newUser.save();
+
+      // Registration successful
+      return res.status(201).json({ message: "Registration successful" });
+    } catch (error) {
+      console.error("Registration error:", error);
+      return res.status(500).json({ error: "User registration failed" });
+    }
+  } else {
+    return res.status(405).json({ error: "Method not allowed" });
   }
 }
--- src/app/components/RegisterForm.jsx
+++ src/app/components/RegisterForm.jsx
@@ -70,6 +70,7 @@ export default function RegisterForm() {
       }
     } catch (error) {
       setError("User registration failed.", error);
+      return res.status(500).json({ error: "User registration failed" });
     }
   };
 
@@ -119,7 +120,7 @@ export default function RegisterForm() {
                   className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   onChange={(e) => setEmail(e.target.value)}
                   type="text"
-                  placeholder="littlelamb@gmail.com"
+                  placeholder="John@gmail.com"
                 />
               </div>
               <label
